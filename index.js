9a6a897cb626b6226e133b1a5e410a12c5196c83
Achal <achalgupta277@gmail.com>
2019-05-26 00:44:18 +0530

    commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var socket = require("socket.io");
+var app = express();
+var PORT = process.env.PORT || 4000;
+var server = app.listen(PORT, function() {
+  console.log("app running");
+});
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to crush");
+});
+
+// socket setup
+
+var io = socket(server);
+
+io.on("connection", function(socket) {
+  console.log(socket.id);
+
+  socket.on("chat", data => {
+    io.sockets.emit("chat", data);
+  });
+
+  socket.on("typing", function(data) {
+    socket.broadcast.emit("typing", data.handle);
+  });
+});
---
+++ public/chat.js
@@ -0,0 +1,33 @@
+// Make connection
+
+var socket = io.connect("http://localhost:4000");
+
+// Query DOM
+var message = document.getElementById("message"),
+  handle = document.getElementById("handle"),
+  btn = document.getElementById("send"),
+  output = document.getElementById("output");
+feedback = document.querySelector("#feedback");
+//Emit events
+
+btn.addEventListener("click", function() {
+  socket.emit("chat", {
+    message: message.value,
+    handle: handle.value
+  });
+});
+
+message.addEventListener("keypress", function() {
+  socket.emit("typing", {
+    handle: handle.value
+  });
+});
+
+socket.on("chat", function(data) {
+  feedback.innerHTML = "";
+  output.innerHTML += `${data.handle} : ${data.message}<br>`;
+});
+
+socket.on("typing", function(data) {
+  feedback.innerHTML = `${data} is typing ..`;
+});
